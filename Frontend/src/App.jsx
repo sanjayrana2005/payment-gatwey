c60ea388d47f1c19e39b0c1c0c92025ad359ec29
Sanjay Rana <sanjayrana5113@gmail.com>
2026-01-03 23:49:02 +0530

    route protected

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,14 +4,32 @@ import Home from './Pages/Home'
 import Login from './Components/Login'
 import Subscription from './Components/Subscription'
 import Signup from './Components/Signup'
+import { Navigate } from "react-router-dom";
 
 const App = () => {
+
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("paymentToken"); // or cookie check
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
-      <Route path='/subscription' element={<Subscription />} />
+      <Route path='/subscription' element={
+        <ProtectedRoute>
+        <Subscription />
+        </ProtectedRoute>
+        } />
     </Routes>
   )
 }
