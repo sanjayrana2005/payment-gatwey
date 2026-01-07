5a22b511f8bbade0ccfa6391e0af13c23444b239
Sanjay Rana <sanjayrana5113@gmail.com>
2026-01-07 22:09:36 +0530

    api call

--- Frontend/src/Components/Subscription.jsx
+++ Frontend/src/Components/Subscription.jsx
@@ -1,5 +1,5 @@
 import { UserRound } from "lucide-react";
-import { data, Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import { toast } from "react-toastify";
@@ -25,6 +25,21 @@ const Subscription = ({ user }) => {
     }
   };
 
+  const handleSubscription = async (planType) =>{
+    try {
+      const {data}=await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/payment/order`,
+        {
+          planType
+        },{
+          withCredentials:true
+        }
+      )
+      console.log(data)
+    } catch (error) {
+      toast.error(error?.response?.data?.message)
+    }
+  }
+
   return (
     <div className="min-h-screen bg-slate-100 flex flex-col">
       <nav className="bg-slate-900 text-white px-6 md:px-10 py-4 flex justify-between items-center">
@@ -32,7 +47,6 @@ const Subscription = ({ user }) => {
           PAYMENT
         </Link>
 
-        {/* USER MENU */}
         <div className="relative">
           <button
             onClick={() => setOpen(!open)}
@@ -44,7 +58,6 @@ const Subscription = ({ user }) => {
             <UserRound className="cursor-pointer"/>
           </button>
 
-          {/* DROPDOWN (PHONE + DESKTOP) */}
           {open && (
             <div className="absolute right-0 mt-2 bg-white text-slate-800 rounded-lg shadow-lg w-36 z-50">
               <button
@@ -58,7 +71,6 @@ const Subscription = ({ user }) => {
         </div>
       </nav>
 
-      {/* ================= PAGE CONTENT ================= */}
       <div className="flex flex-col items-center px-4 py-12">
         {/* Heading */}
         <div className="text-center mb-12">
@@ -86,12 +98,11 @@ const Subscription = ({ user }) => {
               <li>✅ Email Support</li>
             </ul>
 
-            <Link
-              to="/payment"
-              className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 rounded-xl font-semibold transition transform hover:scale-105"
+            <button onClick={()=>handleSubscription("basic")}
+              className="bg-blue-600 hover:bg-blue-700 cursor-pointer text-white py-3 px-8 rounded-xl font-semibold transition transform hover:scale-105"
             >
               Choose Basic
-            </Link>
+            </button>
           </div>
 
           {/* PRO PLAN */}
@@ -109,12 +120,11 @@ const Subscription = ({ user }) => {
               <li>✅ Advanced Analytics</li>
             </ul>
 
-            <Link
-              to="/payment"
-              className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 rounded-xl font-semibold transition transform hover:scale-105"
+            <button onClick={()=>handleSubscription("pro")}
+              className="bg-blue-600 hover:bg-blue-700 cursor-pointer text-white py-3 px-8 rounded-xl font-semibold transition transform hover:scale-105"
             >
               Choose Pro
-            </Link>
+            </button>
           </div>
         </div>
 
--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -37,7 +37,6 @@ const createOrder = async (req, res) => {
         res.status(400).json({
             message: error.message
         });
-        console.log(error)
     }
 }
 
